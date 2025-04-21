2a883846e861b4f12a597b8ba984d35fbc602767
ines03garcia <igcruchinho@gmail.com>
2025-04-21 16:20:02 +0000

    feat: resizing without distortion

--- scripts/preprocess_VinDrMammo.py
+++ scripts/preprocess_VinDrMammo.py
@@ -2,7 +2,7 @@ import os
 import imageio
 import torch
 import numpy as np
-from torchvision import transforms
+import torchvision.transforms.functional as Tf
 from tqdm import tqdm
 from skimage import img_as_ubyte
 from PIL import Image
@@ -13,24 +13,28 @@ input_folder = '/home/csantiago/datasets/Vindir-mammoclip/VinDir_preprocessed_ma
 output_folder = '/home/csantiago/data'
 os.makedirs(output_folder, exist_ok=True)
 
-transform = transforms.Compose([
-    transforms.Resize((256, 256)),
-    transforms.ToTensor(),  # Converts to [0,1] float32 tensor
-])
+target_size = 256
 
 for folder in tqdm(os.listdir(input_folder)):
     folder_path = os.path.join(input_folder, folder)
+    
     for filename in os.listdir(folder_path):
         if not filename.lower().endswith(".png"):
             continue
-    
+            
         image_path = os.path.join(input_folder, folder, filename)
         img = Image.open(image_path)
+
+        w, h = img.size
+        scale = target_size / max(w, h)
+        new_w, new_h = int(w * scale), int(h * scale)
+        img = img.resize((new_w, new_h), Image.BILINEAR)
     
-        img_tensor = transform(img)
-    
-        # Convert back to uint8 (.png)
-        img_uint8 = img_as_ubyte(img_tensor.squeeze().numpy())
+        pad_w = (target_size - new_w) // 2
+        pad_h = (target_size - new_h) // 2
+        padding = (pad_w, pad_h, target_size - new_w - pad_w, target_size - new_h - pad_h)
+        img = Tf.pad(img, padding, fill=0)
     
         output_path = os.path.join(output_folder, filename)
-        imageio.imwrite(output_path, img_uint8)
+        imageio.imwrite(output_path, img)
+
